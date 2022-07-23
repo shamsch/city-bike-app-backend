33896b2e5915bd689eba7c7f975c3828484de79a
Shamsur <shamsur314@gmail.com>
2022-07-23 23:05:23 +0300

    util functions

---
+++ src/utils/convertUnit.ts
@@ -0,0 +1,7 @@
+export const secondsToMinutes = (seconds: number) => {
+    return Math.floor(seconds / 60);
+}
+
+export const metersToKilometers = (meters: number) => {
+    return meters / 1000;
+}
