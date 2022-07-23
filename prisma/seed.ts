059283f4f3f96fa08b52b0e5fa4a7f98fe8cc1d3
Shamsur <shamsur314@gmail.com>
2022-07-23 22:36:29 +0300

    db seeded

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,15 +1,53 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+const csv = require("csv-parser");
+const fs = require("fs");
+const path = require("path");
 
 async function main() {
- 
+    fs.createReadStream(path.join(__dirname, "../data/csv_small/journey.csv"))
+        .pipe(csv())
+        .on("data", async (data: any) => {
+            await prisma.journey.create({
+                data: {
+                    id: Number(data.id),
+                    covered_distance: Number(data.covered_distance),
+                    departure_station: String(data.departure_station),
+                    departure_time: new Date(data.departure_time),
+                    duration: Number(data.duration),
+                    departure_station_id: Number(data.departure_station_id),
+                    month: String(data.month),
+                    return_station: String(data.return_station),
+                    return_time: new Date(data.return_time),
+                    return_station_id: Number(data.return_station_id),
+                },
+            });
+        });
+
+    fs.createReadStream(path.join(__dirname, "../data/csv_small/station.csv"))
+        .pipe(csv())
+        .on("data", async (data: any) => {
+            await prisma.station.create({
+                data: {
+                    id: Number(data.id),
+                    name: String(data.name),
+                    lat: Number(data.lat),
+                    lon: Number(data.lon),
+                    city: String(data.city),
+                    address: String(data.address),
+                    capacity: Number(data.capacity),
+                    operator: String(data.operator),
+                },
+            });
+        });
 }
 
 main()
-  .catch((e) => {
-    console.error(e)
-    process.exit(1)
-  })
-  .finally(async () => {
-    await prisma.$disconnect()
-  })
+    .catch((e) => {
+        console.error(e);
+        process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
