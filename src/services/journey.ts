bbc3c3236dd9e99e3f425b24ee5dd2bb8c7042c7
Shamsur <shamsur314@gmail.com>
2022-07-24 16:24:43 +0300

    searching in journey #1

--- src/services/journey.ts
+++ src/services/journey.ts
@@ -4,15 +4,53 @@ import { metersToKilometers, secondsToMinutes } from "../utils/convertUnit";
 
 const prisma = new PrismaClient();
 
-export const getAllJourneys = async (limit: number, page: number, orderBy: orderByJourney, orderDir: orderDir) => {
-    const offset = (page - 1) * limit;
+export const getAllJourneys = async (
+    page: number,
+    limit: number,
+    orderBy: orderByJourney,
+    orderDir: orderDir,
+    search: string | undefined
+) => {
+    const offset = page === 1 ? 0 : (page - 1) * limit;
+    
+    let where = {};
+    
+    if (search) {
+        where = {
+            OR: [
+                {
+                    month: {
+                        contains: search,
+                        mode: "insensitive"
+                    },
+                },
+                {
+                    departure_station: {
+                        contains: search,
+                        mode: "insensitive"
+                    },
+                },
+                {
+                    return_station: {
+                        contains: search,
+                        mode: "insensitive"
+                    },
+                },
+            ],
+        } 
+    }
+
+    console.log(page, limit, orderBy, orderDir, search);
+
     const journeys = await prisma.journey.findMany({
         take: limit,
         skip: offset,
         orderBy: {
-            [orderBy]: orderDir
-        }
+            [orderBy]: orderDir,
+        },
+        where, 
     });
+
     const convertedJourney = journeys.map((journey) => {
         return {
             ...journey,
