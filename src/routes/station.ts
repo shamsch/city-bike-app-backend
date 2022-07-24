bbaeee36139e46743e0b94f88286c41c501edd87
Shamsur <shamsur314@gmail.com>
2022-07-24 17:15:30 +0300

    page and limit passed in wrong order fix

--- src/routes/station.ts
+++ src/routes/station.ts
@@ -6,7 +6,7 @@ const stationRouter = Router();
 stationRouter.get("/", async (req, res) => {
     const limit = Number(req.query.limit) || 10;
     const page = Number(req.query.page) || 1;
-    const stations = await getAllStations(limit, page);
+    const stations = await getAllStations(page, limit);
     res.json(stations);
 });
 
