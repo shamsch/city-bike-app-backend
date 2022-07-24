456ffd7c9cd4fe1a4ddec74e286a7a2a61e156ea
Shamsur <shamsur314@gmail.com>
2022-07-24 13:51:54 +0300

    validates request queries

---
+++ src/utils/validateJourney.ts
@@ -0,0 +1,41 @@
+import { orderByJourney, orderDir } from "../types";
+
+export const validateOrderBy = (requestOrderBy: string): orderByJourney => {
+    switch (requestOrderBy) {
+        case 'id':
+            return orderByJourney.id;
+        case 'departure_station':
+            return orderByJourney.departure_station;
+        case 'departure_time':
+            return orderByJourney.departure_time;
+        case 'departure_station_id':
+            return orderByJourney.departure_station_id;
+        case 'return_station':
+            return orderByJourney.return_station;
+        case 'return_time':
+            return orderByJourney.return_time;
+        case 'return_station_id':
+            return orderByJourney.return_station_id;
+        case 'duration':
+            return orderByJourney.duration;
+        case 'covered_distance':
+            return orderByJourney.covered_distance;
+        case 'month':
+            return orderByJourney.month;
+        default:
+            return orderByJourney.id;
+    }
+}
+
+
+
+export const validateOrderDir = (requestOrderDir: string): orderDir => {
+    switch (requestOrderDir) {
+        case 'asc':
+            return 'asc';
+        case 'desc':
+            return 'desc';
+        default:
+            return 'desc';
+    }
+}
