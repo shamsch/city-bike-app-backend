70303cade5db43335e51f8f92de43089c7efb9df
Shamsur <shamsur314@gmail.com>
2022-07-24 13:52:20 +0300

    orderBy and order direction done

--- src/routes/journey.ts
+++ src/routes/journey.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
 import { getAllJourneys } from "../services/journey";
+import { JourneyGetRequest, JourneyGetResponse } from "../types";
+import { validateOrderBy, validateOrderDir } from "../utils/validateJourney";
 
 const journeyRouter = Router();
 
-journeyRouter.get("/", async (req, res) => {
+journeyRouter.get("/", async (req: JourneyGetRequest, res: JourneyGetResponse) => {
     const limit = Number(req.query.limit) || 10;
     const page = Number(req.query.page) || 1;
-    const journeys = await getAllJourneys(limit, page);
+    const orderBy = validateOrderBy(req.query.orderBy);
+    const orderDir = validateOrderDir(req.query.orderDir);
+
+
+    const journeys = await getAllJourneys(limit, page, orderBy, orderDir);
+
     res.json(journeys);
 });
 
--- src/services/journey.ts
+++ src/services/journey.ts
@@ -1,13 +1,17 @@
 import { PrismaClient } from "@prisma/client";
+import { orderByJourney, orderDir } from "../types";
 import { metersToKilometers, secondsToMinutes } from "../utils/convertUnit";
 
 const prisma = new PrismaClient();
 
-export const getAllJourneys = async (limit: number, page: number) => {
+export const getAllJourneys = async (limit: number, page: number, orderBy: orderByJourney, orderDir: orderDir) => {
     const offset = (page - 1) * limit;
     const journeys = await prisma.journey.findMany({
         take: limit,
         skip: offset,
+        orderBy: {
+            [orderBy]: orderDir
+        }
     });
     const convertedJourney = journeys.map((journey) => {
         return {
