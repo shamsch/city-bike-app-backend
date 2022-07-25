3e2ab88fb89a632b8a199a6961157940c3989e81
Shamsur <shamsur314@gmail.com>
2022-07-26 00:05:08 +0300

    exporting app for testing and cors for future

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-import { PrismaClient } from '@prisma/client';
 import journeyRouter from './src/routes/journey';
 import stationRouter from './src/routes/station';
+import cors from 'cors';
 
-const prisma = new PrismaClient();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/journey', journeyRouter)
@@ -15,4 +15,4 @@ app.use('/api/station', stationRouter)
 app.listen(4000, () => console.log('Server started on port 4000'));
 
 // test issue
-
+export default app;
