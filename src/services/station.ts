4ab5b4144709c9d946ae5dbcf6b47aebcd04c11a
Shamsur <shamsur314@gmail.com>
2022-07-25 13:34:56 +0300

    top five journey and avg journey works

--- src/services/station.ts
+++ src/services/station.ts
@@ -1,5 +1,6 @@
 import {PrismaClient} from '@prisma/client';
 import { count } from 'console';
+import { metersToKilometers } from '../utils/convertUnit';
 
 const prisma = new PrismaClient();
 
@@ -28,7 +29,7 @@ export const getAllStations = async (page: number, limit: number, search: string
 export const getStationById = async (id: number) => {
     const station = await prisma.station.findUnique({
         where: {
-            id
+            id, 
         }, 
         select:{
             id: true, 
@@ -36,23 +37,60 @@ export const getStationById = async (id: number) => {
             address: true,
             departure_journey: {
                 select: {
-                    id: true,
+                    covered_distance: true,
+                    departure_station: true,
+                    return_station: true,
                 }
             }, 
             return_journey: {
                 select: {
-                    id: true,
+                    covered_distance: true,
+                    departure_station: true,
+                    return_station: true,
                 }
             }
-        }, 
+        },
     });
 
     const totalDepartureJourneys = station?.departure_journey?.length || 0;
     const totalReturnJourneys = station?.return_journey?.length || 0;
 
+    const averageDepartureDistance = (station?.departure_journey?.reduce((acc, curr) => acc + curr.covered_distance, 0) || 0) / totalDepartureJourneys;
+    const averageReturnDistance = (station?.return_journey?.reduce((acc, curr) => acc + curr.covered_distance, 0) || 0) / totalReturnJourneys;
+
+    const topDepartureStation = station?.departure_journey?.reduce((acc, curr) => {
+        if(!acc[curr.return_station]) {
+            acc[curr.return_station] = 1;
+        }
+        else {
+            acc[curr.return_station] += 1;
+        }
+        return acc;
+    }
+    , {}) || {};
+
+    const topDepartureStationName = Object.keys(topDepartureStation).sort((a, b) => topDepartureStation[b] - topDepartureStation[a]).slice(0, 5);
+
+    const topReturnStation = station?.return_journey?.reduce((acc, curr) => {
+        if(!acc[curr.departure_station]) {
+            acc[curr.departure_station] = 1;
+        }
+        else {
+            acc[curr.departure_station] += 1;
+        }
+        return acc;
+    }
+    , {}) || {};
+
+    const topReturnStationName = Object.keys(topReturnStation).sort((a, b) => topReturnStation[b] - topReturnStation[a]).slice(0, 5);
+
     return {
         ...station,
         departure_journey: totalDepartureJourneys,
         return_journey: totalReturnJourneys,
+        average_departure_distance: metersToKilometers(averageDepartureDistance),
+        average_return_distance: metersToKilometers(averageReturnDistance),
+        top_departure_station: topDepartureStationName,
+        top_return_station: topReturnStationName,
     }
 }
