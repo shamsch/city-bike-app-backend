ce4d96c9a06d5e98366d5f612ccb8d1b0aff056a
Shamsur <shamsur314@gmail.com>
2022-07-26 23:32:20 +0300

    for heroku deployment

--- index.ts
+++ index.ts
@@ -11,8 +11,9 @@ app.use(express.json());
 app.use('/api/journey', journeyRouter)
 app.use('/api/station', stationRouter)
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(4000, () => console.log('Server started on port 4000'));
+app.listen(PORT, () => console.log('Server started on port 4000'));
 
 // test issue
 export default app;
