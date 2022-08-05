e6cd82883f9a4a6cc1f4ed14827a8bfc03a6c2bc
Shamsur Chowdhury <shamsur.chowdhury@crowdsorsa.com>
2022-08-05 12:22:28 +0300

    deploy to heroku

--- index.ts
+++ index.ts
@@ -1,19 +1,19 @@
-import express from 'express';
-import journeyRouter from './src/routes/journey';
-import stationRouter from './src/routes/station';
-import cors from 'cors';
+import express from "express";
+import journeyRouter from "./src/routes/journey";
+import stationRouter from "./src/routes/station";
+import cors from "cors";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/journey', journeyRouter)
-app.use('/api/station', stationRouter)
+app.use("/api/journey", journeyRouter);
+app.use("/api/station", stationRouter);
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => console.log('Server started on port ' + PORT));
+app.listen(PORT, () => console.log("Server started on port " + PORT));
 
-// test issue
+// deploy to heroku
 export default app;
