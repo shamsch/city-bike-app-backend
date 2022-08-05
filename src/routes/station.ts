d6895d039971185844f39079d15b7b69beb71de2
Shamsur Chowdhury <shamsur.chowdhury@crowdsorsa.com>
2022-08-05 11:01:53 +0300

    removed debugging logs and formatting

--- src/routes/station.ts
+++ src/routes/station.ts
@@ -1,71 +1,73 @@
 import { Router } from "express";
 import { body, validationResult } from "express-validator";
-import { addStation, getAllStations, getStationById, getStationOptions } from "../services/station";
 import {
-    StationGetRequest,
-    StationGetResponse,
-    StationGetByIdRequest,
-    StationGetByIdResponse,
-    StationPostRequest,
-    StationPostResponse,
+	addStation,
+	getAllStations,
+	getStationById,
+	getStationOptions,
+} from "../services/station";
+import {
+	StationGetRequest,
+	StationGetResponse,
+	StationGetByIdRequest,
+	StationGetByIdResponse,
+	StationPostRequest,
+	StationPostResponse,
 } from "../types";
 
 const stationRouter = Router();
 
 stationRouter.get(
-    "/",
-    async (req: StationGetRequest, res: StationGetResponse) => {
-        const limit = Number(req.query.limit) || 10;
-        const page = Number(req.query.page) || 1;
-        const search = req.query.search;
-        const stations = await getAllStations(page, limit, search);
-        res.json(stations);
-    }
+	"/",
+	async (req: StationGetRequest, res: StationGetResponse) => {
+		const limit = Number(req.query.limit) || 10;
+		const page = Number(req.query.page) || 1;
+		const search = req.query.search;
+		const stations = await getAllStations(page, limit, search);
+		res.json(stations);
+	}
 );
 
 stationRouter.get("/stationOptions", async (req, res) => {
-    const stations = await getStationOptions();
-    res.json(stations);
-}
-);
+	const stations = await getStationOptions();
+	res.json(stations);
+});
 
 stationRouter.get(
-    "/:id",
-    async (req: StationGetByIdRequest, res: StationGetByIdResponse) => {
-        const id = Number(req.params.id);
-        const month = req.query.month? String(req.query.month) : undefined;
+	"/:id",
+	async (req: StationGetByIdRequest, res: StationGetByIdResponse) => {
+		const id = Number(req.params.id);
+		const month = req.query.month ? String(req.query.month) : undefined;
 
-        if (isNaN(id)) {
-            res.status(400).json({ error: "Invalid id" });
-        } else {
-            const station = await getStationById(id, month);
-            if (!station) {
-                res.status(404).json({ error: "Station not found" });
-            } else {
-                res.json(station);
-            }
-        }
-    }
+		if (isNaN(id)) {
+			res.status(400).json({ error: "Invalid id" });
+		} else {
+			const station = await getStationById(id, month);
+			if (!station) {
+				res.status(404).json({ error: "Station not found" });
+			} else {
+				res.json(station);
+			}
+		}
+	}
 );
 
-
-stationRouter.post("/add", 
-    body("name").isString().isLength({ min: 1 }),
-    body("address").isString().isLength({ min: 1 }),
-    body("lat").isFloat(),
-    body("lon").isFloat(),
-    body("capacity").isInt(),
-    async (req: StationPostRequest, res: StationPostResponse) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            res.status(400).json({ error: errors.array() });
-        } else {
-            const { name, address, lat, lon, capacity } = req.body;
-            const station = await addStation(name, address, lat, lon, capacity);
-            res.json(station);
-        }
-    }
-
-
-)
+stationRouter.post(
+	"/add",
+	body("name").isString().isLength({ min: 1 }),
+	body("address").isString().isLength({ min: 1 }),
+	body("lat").isFloat(),
+	body("lon").isFloat(),
+	body("capacity").isInt(),
+	async (req: StationPostRequest, res: StationPostResponse) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			res.status(400).json({ error: errors.array() });
+		} else {
+			const { name, address, lat, lon, capacity } = req.body;
+			const station = await addStation(name, address, lat, lon, capacity);
+			res.json(station);
+		}
+	}
+);
 export default stationRouter;
--- src/services/journey.ts
+++ src/services/journey.ts
@@ -1,167 +1,163 @@
 import { Journey, PrismaClient } from "@prisma/client";
 import { orderByJourney, orderDir } from "../types";
 import {
-    metersToKilometers,
-    secondsToMinutes,
-    kilometersToMeters,
-    minutesToSeconds,
+	metersToKilometers,
+	secondsToMinutes,
+	kilometersToMeters,
+	minutesToSeconds,
 } from "../utils/convertUnit";
 
 const prisma = new PrismaClient();
 
 export const getAllJourneys = async (
-    page: number,
-    limit: number,
-    orderBy: orderByJourney,
-    orderDir: orderDir,
-    search: string | undefined,
-    durationMin: number,
-    durationMax: number,
-    distanceMin: number,
-    distanceMax: number
+	page: number,
+	limit: number,
+	orderBy: orderByJourney,
+	orderDir: orderDir,
+	search: string | undefined,
+	durationMin: number,
+	durationMax: number,
+	distanceMin: number,
+	distanceMax: number
 ) => {
-    const cursorId = page === 1 ? 1 : (page - 1) * limit + 1;
-    const offset = (page - 1) * limit;
+	const cursorId = page === 1 ? 1 : (page - 1) * limit + 1;
+	const offset = (page - 1) * limit;
 
-    let searchQuery = {};
-    let durationQuery = {};
-    let distanceQuery = {};
+	let searchQuery = {};
+	let durationQuery = {};
+	let distanceQuery = {};
 
-    if (durationMax > 0) {
-        durationQuery = {
-            duration: {
-                gte: minutesToSeconds(durationMin),
-                lte: minutesToSeconds(durationMax),
-            },
-        };
-    }
+	if (durationMax > 0) {
+		durationQuery = {
+			duration: {
+				gte: minutesToSeconds(durationMin),
+				lte: minutesToSeconds(durationMax),
+			},
+		};
+	}
 
-    if (distanceMax > 0) {
-        distanceQuery = {
-            covered_distance: {
-                gte: kilometersToMeters(distanceMin),
-                lte: kilometersToMeters(distanceMax),
-            },
-        };
-    }
+	if (distanceMax > 0) {
+		distanceQuery = {
+			covered_distance: {
+				gte: kilometersToMeters(distanceMin),
+				lte: kilometersToMeters(distanceMax),
+			},
+		};
+	}
 
-    if (search) {
-        searchQuery = {
-            OR: [
-                {
-                    month: {
-                        contains: search,
-                        mode: "insensitive",
-                    },
-                },
-                {
-                    departure_station: {
-                        contains: search,
-                        mode: "insensitive",
-                    },
-                },
-                {
-                    return_station: {
-                        contains: search,
-                        mode: "insensitive",
-                    },
-                },
-            ],
-        };
-    }
+	if (search) {
+		searchQuery = {
+			OR: [
+				{
+					month: {
+						contains: search,
+						mode: "insensitive",
+					},
+				},
+				{
+					departure_station: {
+						contains: search,
+						mode: "insensitive",
+					},
+				},
+				{
+					return_station: {
+						contains: search,
+						mode: "insensitive",
+					},
+				},
+			],
+		};
+	}
 
-    const where = {
-        ...searchQuery,
-        ...durationQuery,
-        ...distanceQuery,
-    };
+	const where = {
+		...searchQuery,
+		...durationQuery,
+		...distanceQuery,
+	};
 
-    let journeys:Journey[] = []; 
+	let journeys: Journey[] = [];
 
-    if (orderBy === "id" && orderDir === "asc") {
-         journeys = await prisma.journey.findMany({
-            take: limit,
-            cursor: {
-                id: cursorId,
-            },
-            where,
-        });
-    }
-    else {
-        journeys = await prisma.journey.findMany({
-            take: limit,
-            skip: offset,
-            orderBy: {
-                [orderBy]: orderDir,
-            },
-            where,
-        });
-    }
+	if (orderBy === "id" && orderDir === "asc") {
+		journeys = await prisma.journey.findMany({
+			take: limit,
+			cursor: {
+				id: cursorId,
+			},
+			where,
+		});
+	} else {
+		journeys = await prisma.journey.findMany({
+			take: limit,
+			skip: offset,
+			orderBy: {
+				[orderBy]: orderDir,
+			},
+			where,
+		});
+	}
 
-    const convertedJourney = journeys.map((journey) => {
-        return {
-            ...journey,
-            duration: secondsToMinutes(journey.duration),
-            covered_distance: metersToKilometers(journey.covered_distance),
-        };
-    });
+	const convertedJourney = journeys.map((journey) => {
+		return {
+			...journey,
+			duration: secondsToMinutes(journey.duration),
+			covered_distance: metersToKilometers(journey.covered_distance),
+		};
+	});
 
-    const totalJourney = await prisma.journey.count({
-        where,
-    });
-    const totalPages = Math.ceil(totalJourney / limit);
+	const totalJourney = await prisma.journey.count({
+		where,
+	});
+	const totalPages = Math.ceil(totalJourney / limit);
 
-    return {
-        journeys: convertedJourney,
-        total_pages: totalPages,
-    };
+	return {
+		journeys: convertedJourney,
+		total_pages: totalPages,
+	};
 };
 
 export const addJourney = async (
-    month: string,
-    departure_station: number, 
-    return_station: number,
-    duration: number,
-    covered_distance: number,
-    departure_time: Date, 
-    return_time: Date
+	month: string,
+	departure_station: number,
+	return_station: number,
+	duration: number,
+	covered_distance: number,
+	departure_time: Date,
+	return_time: Date
 ) => {
-    console.log(departure_station, return_station, );
-    // find departure station and return station in database
-    const departureStation = await prisma.station.findUnique({
-        where: {
-            id: departure_station,
-        },
-    });
-    const returnStation = await prisma.station.findUnique({
-        where: {
-            id: return_station,
-        },
-    });
+	// find departure station and return station in database
+	const departureStation = await prisma.station.findUnique({
+		where: {
+			id: departure_station,
+		},
+	});
+	const returnStation = await prisma.station.findUnique({
+		where: {
+			id: return_station,
+		},
+	});
 
-    if (!departureStation || !returnStation) {
-        return false; 
-    }
+	if (!departureStation || !returnStation) {
+		return false;
+	}
 
-    const journey = await prisma.journey.create({
-        data: {
-            departure_station_id: departureStation.id,
-            return_station_id: returnStation.id,
-            departure_station: departureStation.name, 
-            return_station: returnStation.name,
-            departure_time: new Date(departure_time),
-            return_time: new Date(return_time),
-            duration: duration, // already in seconds because of the conversion routes/journey.ts
-            covered_distance: kilometersToMeters(covered_distance),
-            month,
-        },
-    });
+	const journey = await prisma.journey.create({
+		data: {
+			departure_station_id: departureStation.id,
+			return_station_id: returnStation.id,
+			departure_station: departureStation.name,
+			return_station: returnStation.name,
+			departure_time: new Date(departure_time),
+			return_time: new Date(return_time),
+			duration: duration, // already in seconds because of the conversion routes/journey.ts
+			covered_distance: kilometersToMeters(covered_distance),
+			month,
+		},
+	});
 
-    if (!journey) {
-        return false;
-    }
-    else {
-        return journey; 
-    }
-}
-
+	if (!journey) {
+		return false;
+	} else {
+		return journey;
+	}
+};
