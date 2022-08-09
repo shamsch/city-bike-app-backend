9f0c4592de9b9f2e2122e3815376dd01630dfe2f
Shamsur Chowdhury <shamsur.chowdhury@crowdsorsa.com>
2022-08-09 16:12:22 +0300

    added more tests post req on station endpoint

--- src/__tests__/station_post.test.ts
+++ src/__tests__/station_post.test.ts
@@ -2,20 +2,125 @@ import { expect } from "chai";
 import "mocha";
 
 import app from "../..";
-
+import "dotenv/config";
 import request from "supertest";
+import { after } from "mocha";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 describe("POST /station/add", () => {
-	it("adding without pass fails", async () => {
-		const response = await request(app).post("/api/station/add").send({
-			name: "Laivasillankatu",
-			address: "Laivasillankatu 14",
-			capacity: 12,
-			lon: 24.9565097715858,
-			lat: 60.1609890692806,
+	after(async () => {
+		const res = await prisma.station.deleteMany({
+			where: {
+				name: "TestStation",
+			},
+		});
+		console.log("Number of deleted stations: ", res.count);
+		prisma.$disconnect();
+	}),
+		it("adding without pass fails", async () => {
+			const response = await request(app).post("/api/station/add").send({
+				name: "TestStation",
+				address: "TestAddress 123",
+				capacity: 100,
+				lon: 24.95,
+				lat: 60.16,
+			});
+
+			expect(response.body.error).to.equal("No pass provided");
+			expect(response.status).to.equal(401);
 		});
 
-		expect(response.body.error).to.equal("No pass provided");
-		expect(response.status).to.equal(401);
+	it("adding with pass succeeds", async () => {
+		const response = await request(app)
+			.post("/api/station/add")
+			.send({
+				name: "TestStation",
+				address: "TestAddress 123",
+				capacity: 100,
+				lon: 24.95,
+				lat: 60.16,
+			})
+			.set("pass", process.env.PASS);
+
+		expect(response.body.error).to.equal(undefined);
+		expect(response.status).to.equal(200);
+		expect(response.body).to.contain.keys([
+			"id",
+			"name",
+			"address",
+			"capacity",
+			"lon",
+			"lat",
+		]);
+	});
+
+	it("adding with missing name fails", async () => {
+		const response = await request(app)
+			.post("/api/station/add")
+			.send({
+				address: "TestAddress 123",
+				capacity: 100,
+				lon: 24.95,
+				lat: 60.16,
+			})
+			.set("pass", process.env.PASS);
+		expect(response.body.error[0].msg).to.equal("Invalid value");
+		expect(response.status).to.equal(400);
+	});
+
+	it("adding with missing address fails", async () => {
+		const response = await request(app)
+			.post("/api/station/add")
+			.send({
+				name: "TestStation",
+				capacity: 100,
+				lon: 24.95,
+				lat: 60.16,
+			})
+			.set("pass", process.env.PASS);
+		expect(response.body.error[0].msg).to.equal("Invalid value");
+		expect(response.status).to.equal(400);
+	}),
+		it("adding with missing capacity fails", async () => {
+			const response = await request(app)
+				.post("/api/station/add")
+				.send({
+					name: "TestStation",
+					address: "TestAddress 123",
+					lon: 24.95,
+					lat: 60.16,
+				})
+				.set("pass", process.env.PASS);
+			expect(response.body.error[0].msg).to.equal("Invalid value");
+			expect(response.status).to.equal(400);
+		}),
+		it("adding with missing lat and lon fails", async () => {
+			const response = await request(app)
+				.post("/api/station/add")
+				.send({
+					name: "TestStation",
+					address: "TestAddress 123",
+					capacity: 100,
+				})
+				.set("pass", process.env.PASS);
+			expect(response.body.error[0].msg).to.equal("Invalid value");
+			expect(response.status).to.equal(400);
+		});
+
+	it("adding with invalid capacity fails", async () => {
+		const response = await request(app)
+			.post("/api/station/add")
+			.send({
+				name: "TestStation",
+				address: "TestAddress 123",
+				capacity: -1, //must be non-negative integer
+				lon: 24.95,
+				lat: 60.16,
+			})
+			.set("pass", process.env.PASS);
+		expect(response.body.error[0].msg).to.equal("Invalid value");
+		expect(response.status).to.equal(400);
 	});
 });
