c6d2b0d7961a7d152e682732a9f25dcf8142ce94
Shamsur <shamsur314@gmail.com>
2022-08-09 22:27:51 +0300

    added test for journey post endpoint fixes #20

--- src/__tests__/journeyPost.test.ts
+++ src/__tests__/journeyPost.test.ts
@@ -13,7 +13,7 @@ describe("POST /journey/add", () => {
 	after(async () => {
 		const res = await prisma.journey.deleteMany({
 			where: {
-				covered_distance: 666.555,
+				covered_distance: 666555,
 			},
 		});
 		console.log("Number of deleted journeys: ", res.count);
@@ -23,8 +23,8 @@ describe("POST /journey/add", () => {
 			const response = await request(app).post("/api/journey/add").send({
 				departure_time: "2021-05-31T23:57:25.000Z",
 				return_time: "2021-06-01T00:05:46.000Z",
-				departure_station_id: 1,
-				return_station_id: 2,
+				departure_station: 1,
+				return_station: 2,
 				covered_distance: 666.555,
 				month: "January",
 			});
@@ -39,13 +39,87 @@ describe("POST /journey/add", () => {
 			.send({
 				departure_time: "2021-05-31T23:57:25.000Z",
 				return_time: "2021-06-01T00:05:46.000Z",
-				departure_station_id: 1,
-				return_station_id: 1,
+				departure_station: 1,
+				return_station: 1,
 				covered_distance: 666.555,
 				month: "January",
 			})
 			.set("pass", process.env.PASS);
-		console.log(response.body);
 		expect(response.status).to.equal(200);
+		expect(response.body).to.have.keys(
+			"id",
+			"departure_station",
+			"return_station",
+			"covered_distance",
+			"departure_time",
+			"return_time",
+			"month",
+			"duration",
+			"departure_station_id",
+			"return_station_id",
+		);
+	});
+
+	it("adding with invalid station id fails", async () => {
+		const response = await request(app)
+			.post("/api/journey/add")
+			.send({
+				departure_time: "2021-05-31T23:57:25.000Z",
+				return_time: "2021-06-01T00:05:46.000Z",
+				departure_station: 66666666666666666,
+				return_station: 999999999999999999999,
+				covered_distance: 666.555,
+				month: "January",
+			})
+			.set("pass", process.env.PASS);
+		expect(response.status).to.equal(400);
+		expect(response.body.error).to.equal("Invalid data received");
+	});
+
+	it("adding with invalid month fails", async () => {
+		const response = await request(app)
+			.post("/api/journey/add")
+			.send({
+				departure_time: "2021-05-31T23:57:25.000Z",
+				return_time: "2021-06-01T00:05:46.000Z",
+				departure_station: 1,
+				return_station: 1,
+				covered_distance: 666.555,
+				month: "Not a month",
+			})
+			.set("pass", process.env.PASS);
+		expect(response.status).to.equal(400);
+		expect(response.body.error).to.equal("Invalid data received");
+	});
+
+	it("adding with invalid covered distance fails", async () => {
+		const response = await request(app)
+			.post("/api/journey/add")
+			.send({
+				departure_time: "2021-05-31T23:57:25.000Z",
+				return_time: "2021-06-01T00:05:46.000Z",
+				departure_station: 1,
+				return_station: 1,
+				covered_distance: -666.555,
+				month: "January",
+			})
+			.set("pass", process.env.PASS);
+		expect(response.status).to.equal(400);
+		expect(response.body.error).to.equal("Invalid data received");
+	});
+	it("adding with invalid time fails", async () => {
+		const response = await request(app)
+			.post("/api/journey/add")
+			.send({
+				departure_time: "NOT A TIME",
+				return_time: "NOT A TIME",
+				departure_station: 1,
+				return_station: 1,
+				covered_distance: 666.555,
+				month: "January",
+			})
+			.set("pass", process.env.PASS);
+		expect(response.status).to.equal(400);
+		expect(response.body.error).to.equal("Invalid data received");
 	});
 });
