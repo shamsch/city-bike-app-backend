157d33fc9f4a443c4dd767c8791e2de836b484e8
Shamsur Chowdhury <shamsur.chowdhury@crowdsorsa.com>
2022-08-10 13:12:42 +0300

    added jsdoc function description for utility functions

--- src/utils/staticMapUrl.ts
+++ src/utils/staticMapUrl.ts
@@ -1,3 +1,10 @@
-export const getStaticMapUrl = (lat:Number, lon: Number) => {
-    return  `https://static-maps.yandex.ru/1.x/?lang=en-US&ll=${lon},${lat}&z=12&l=map&size=600,300&pt=${lon},${lat},flag`
-}
+/**
+ * Returns a static map url for the given lat/lng coordinates using the Yandex Static Maps API.
+ * @param {Number} lat
+ * @param {Number} lon
+ * @returns url String
+ */
+
+export const getStaticMapUrl = (lat: Number, lon: Number) => {
+	return `https://static-maps.yandex.ru/1.x/?lang=en-US&ll=${lon},${lat}&z=12&l=map&size=600,300&pt=${lon},${lat},flag`;
+};
