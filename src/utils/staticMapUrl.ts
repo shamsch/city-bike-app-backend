245d534fac2480636f91db1a546e1c9a79f51294
Shamsur Chowdhury <shamsur.chowdhury@crowdsorsa.com>
2022-08-10 13:22:15 +0300

    zoomed in further fixes #11

--- src/utils/staticMapUrl.ts
+++ src/utils/staticMapUrl.ts
@@ -6,5 +6,5 @@
  */
 
 export const getStaticMapUrl = (lat: Number, lon: Number) => {
-	return `https://static-maps.yandex.ru/1.x/?lang=en-US&ll=${lon},${lat}&z=12&l=map&size=600,300&pt=${lon},${lat},flag`;
+	return `https://static-maps.yandex.ru/1.x/?lang=en-US&ll=${lon},${lat}&z=16&l=map&size=600,300&pt=${lon},${lat},flag`;
 };
