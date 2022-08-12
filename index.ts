f3d0db882c701e123bdfcb8cc700640cd5b30d63
Shamsur Chowdhury <shamsur.chowdhury@crowdsorsa.com>
2022-08-12 13:55:58 +0300

    added morgan for logging

--- index.ts
+++ index.ts
@@ -2,11 +2,13 @@ import express from "express";
 import journeyRouter from "./src/routes/journey";
 import stationRouter from "./src/routes/station";
 import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.use("/api/journey", journeyRouter);
 app.use("/api/station", stationRouter);
