b38974444bec013348d46fc100f68cd19b44a5c0
Shamsur <shamsur314@gmail.com>
2022-08-13 16:48:56 +0300

    Add comments Fixes #22

--- src/services/journey.ts
+++ src/services/journey.ts
@@ -9,6 +9,20 @@ import {
 
 const prisma = new PrismaClient();
 
+/**
+ * Get all journeys, paginated
+ * @param page page number
+ * @param limit number of items per page
+ * @param orderBy order by field
+ * @param orderDir order direction
+ * @param search search string
+ * @param durationMin minimum duration
+ * @param durationMax maximum duration
+ * @param distanceMin minimum distance
+ * @param distanceMax maximum distance
+ * @returns {Promise<{journeys: Journey[], total_pages: number}>}
+ */
+
 export const getAllJourneys = async (
 	page: number,
 	limit: number,
@@ -20,6 +34,12 @@ export const getAllJourneys = async (
 	distanceMin: number,
 	distanceMax: number
 ) => {
+	// pagination is done in two ways 
+	// 1. using the offset and limit parameters approach
+	// 2. using the cursor approach
+	// the cursor approach is more efficient at scale if we are ordering by the index field (id)
+	// but for the other fields we need to use the offset and limit approach to paginate
+	// read more about it here: https://www.prisma.io/docs/concepts/components/prisma-client/pagination
 	const cursorId = page === 1 ? 1 : (page - 1) * limit + 1;
 	const offset = (page - 1) * limit;
 
@@ -102,6 +122,7 @@ export const getAllJourneys = async (
 
 	let journeys: Journey[] = [];
 
+	// if we are ordering by the index field (id) we can use the cursor approach as it is more efficient
 	if (orderBy === "id" && orderDir === "asc") {
 		journeys = await prisma.journey.findMany({
 			take: limit,
@@ -110,7 +131,9 @@ export const getAllJourneys = async (
 			},
 			where,
 		});
-	} else {
+	} 
+	// if we are ordering by other fields we need to use the offset and limit approach
+	else {
 		journeys = await prisma.journey.findMany({
 			take: limit,
 			skip: offset,
@@ -121,6 +144,7 @@ export const getAllJourneys = async (
 		});
 	}
 
+	// formats the journeys
 	const convertedJourney = journeys.map((journey) => {
 		return {
 			...journey,
@@ -129,6 +153,7 @@ export const getAllJourneys = async (
 		};
 	});
 
+	// gets the total number of journeys for pagination on the frontend
 	const totalJourney = await prisma.journey.count({
 		where,
 	});
@@ -139,6 +164,16 @@ export const getAllJourneys = async (
 		total_pages: totalPages,
 	};
 };
+
+/**
+ * Add a new journey
+ * @param month month of the journey
+ * @param departure_station departure station ID of the journey
+ * @param return_station return station ID of the journey
+ * @param duration duration of the journey
+ * @param covered_distance covered distance of the journey
+ * @returns {Promise<Journey>}
+ */
 
 export const addJourney = async (
 	month: string,
@@ -185,6 +220,11 @@ export const addJourney = async (
 		return journey;
 	}
 };
+
+/**
+ * get the max and min values for the duration and distance
+ * @returns {Promise<{maxDuration: number, maxDistance: number;}>}
+ */
 
 export const getMaxValues = async () => {
 	const maxDuration = await prisma.journey.aggregate({
--- src/services/station.ts
+++ src/services/station.ts
@@ -5,6 +5,13 @@ import { getStaticMapUrl } from '../utils/staticMapUrl';
 
 const prisma = new PrismaClient();
 
+/**
+ * Get all stations, paginated
+ * @param page page number
+ * @param limit number of items per page
+ * @param search search string station name
+ * @returns {Promise<{stations: Station[], total_pages: number}>}
+ */
 export const getAllStations = async (page: number, limit: number, search: string|undefined) => {
     const offset = (page - 1) * limit;
 
@@ -18,13 +25,13 @@ export const getAllStations = async (page: number, limit: number, search: string
             }
         }
     }
-
+    // we use LIMIT and OFFSET to paginate the results here as the number of stations are not very large
     const stations = await prisma.station.findMany({
         take: limit,
         skip: offset, 
         where
     });
-
+    // we use COUNT to get the total number of stations, for pagination on the frontend
     const totalStations = await prisma.station.count({
         where
     });
@@ -35,7 +42,12 @@ export const getAllStations = async (page: number, limit: number, search: string
         total_pages: totalPages
     };
 }
-
+/**
+ * Get station by id
+ * @param id station id
+ * @param month month to filter all calculations
+ * @returns {Promise<Station>}
+ */
 export const getStationById = async (id: number, month: String | undefined) => {
     
     let monthQuery = {};
@@ -48,7 +60,7 @@ export const getStationById = async (id: number, month: String | undefined) => {
         }
     }
 
-        
+    //using the foreign key to get the all the journeys of the station  
     const station = await prisma.station.findUnique({
         where:{
             id,
@@ -86,14 +98,18 @@ export const getStationById = async (id: number, month: String | undefined) => {
         return null;
     }   
 
+    // a static map url is created for the station
     const staticMapUrl = getStaticMapUrl(station.lat, station.lon);
 
+    // total journey calculation
     const totalDepartureJourneys = station?.departure_journey?.length || 0;
     const totalReturnJourneys = station?.return_journey?.length || 0;
 
+    // average journey distance calculation
     const averageDepartureDistance = (station?.departure_journey?.reduce((acc, curr) => acc + curr.covered_distance, 0) || 0) / totalDepartureJourneys;
     const averageReturnDistance = (station?.return_journey?.reduce((acc, curr) => acc + curr.covered_distance, 0) || 0) / totalReturnJourneys;
 
+    // top five journey to and from station calculation
     const topDepartureStation = station?.departure_journey?.reduce((acc, curr) => {
         if(!acc[curr.return_station]) {
             acc[curr.return_station] = 1;
@@ -132,6 +148,15 @@ export const getStationById = async (id: number, month: String | undefined) => {
     }
 }
 
+/**
+ * Add a station
+ * @param name station name
+ * @param address station address
+ * @param lat station latitude
+ * @param lon station longitude
+ * @param capacity station capacity
+ * @returns {Promise<Station>}
+ */
 export const addStation = async (name: string, address: string, lat: number, lon: number, capacity: number) => {
     const latitude = Number(lat);
     const longitude = Number(lon);
@@ -149,6 +174,10 @@ export const addStation = async (name: string, address: string, lat: number, lon
     return station;
 }
 
+/**
+ * Returns a list of ALL stations  with id and name
+ * @return {Promise<{id, name}[]>}
+ */
 export const getStationOptions = async () => {
     const stations = await prisma.station.findMany({
         select: {
